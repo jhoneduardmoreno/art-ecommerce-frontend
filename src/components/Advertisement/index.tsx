25a1847ca16398b5109412400d8e9494a132e7e8
jhoneduardmoreno <jhoneduar1994@hotmail.com>
2024-04-06 18:41:51 -0500

    Add transition to paragraph in advertisement

--- src/components/Advertisement/index.tsx
+++ src/components/Advertisement/index.tsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
 import styles from './Advertisement.module.scss';
 
 export const Advertisement = () => {
+  const [text, setText] = useState('Conoce los proyectos de Ismara Dayana');
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(true); // Start the fade
+      setTimeout(() => {
+        setText((currentText) =>
+          currentText === 'Conoce los proyectos de Ismara Dayana'
+            ? 'Conoce la nueva colección'
+            : 'Conoce los proyectos de Ismara Dayana'
+        );
+        setFade(false); // Reset fade state for the next cycle
+      }, 500); // Change the text after 0.5 seconds
+    }, 8000); // Total interval, adjust as needed
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={styles.advertisement}>
-      <p>Conoce los proyectos de ismara dayana</p>
+      <p className={`${fade ? styles.fade : ''}`}>{text}</p>
     </div>
-  )
-}
+  );
+};
