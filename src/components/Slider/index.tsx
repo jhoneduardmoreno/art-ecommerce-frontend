43c19675607febb2f2514b0dc0e992637ce1d68c
jhoneduardmoreno <jhoneduar1994@hotmail.com>
2024-04-13 14:14:04 -0500

    Change position of bubbles in sliders

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -37,54 +37,67 @@ const Slider: React.FC = () => {
           position: 'relative',
           width: '100%',
           paddingTop: '66.67%',
-          overflow: 'hidden',
         }}
       >
-        {passportImages.map((image, index) => (
-          <Slide
-            key={image.id}
-            direction={index < activeIndex ? 'right' : 'left'}
-            in={index === activeIndex}
-            mountOnEnter
-            unmountOnExit
-          >
-            <img
-              src={image.url}
-              alt={`Passport ${image.id}`}
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+          }}
+        >
+          {passportImages.map((image, index) => (
+            <Slide
+              key={image.id}
+              direction={index < activeIndex ? 'right' : 'left'}
+              in={index === activeIndex}
+              mountOnEnter
+              unmountOnExit
+            >
+              <img
+                src={image.url}
+                alt={`Passport ${image.id}`}
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            </Slide>
+          ))}
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 16,
+            left: '50%',
+            transform: 'translateX(-50%)',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          {passportImages.map((_, index) => (
+            <div
+              key={index}
               style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover',
+                width: 10,
+                height: 10,
+                borderRadius: '50%',
+                backgroundColor: index === activeIndex ? '#3f51b5' : '#bdbdbd',
+                margin: '0 8px',
+                cursor: 'pointer',
+                transition: 'background-color 0.3s ease-in-out',
               }}
+              onClick={() => handleDotClick(index)}
             />
-          </Slide>
-        ))}
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          marginTop: 16,
-        }}
-      >
-        {passportImages.map((_, index) => (
-          <div
-            key={index}
-            style={{
-              width: 10,
-              height: 10,
-              borderRadius: '50%',
-              backgroundColor: index === activeIndex ? '#3f51b5' : '#bdbdbd',
-              margin: '0 8px',
-              cursor: 'pointer',
-              transition: 'background-color 0.3s ease-in-out',
-            }}
-            onClick={() => handleDotClick(index)}
-          />
-        ))}
+          ))}
+        </div>
       </div>
     </Box>
   );
