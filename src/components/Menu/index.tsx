ddff845e309b3d25c2b6031e8d976fa9d74b1ded
jhoneduardmoreno <jhoneduar1994@hotmail.com>
2024-04-13 11:36:28 -0500

    Add icon button in Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,12 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { IconButton } from '@mui/material';
+
 export const Menu = () => {
   return (
-    <div>Menu</div>
+    <div>
+        <IconButton onClick={() => console.log('Opening menu')}>
+            <MenuIcon sx={{fontSize: '3rem', color: '#000'}} />
+        </IconButton>
+    </div>
   )
 }
