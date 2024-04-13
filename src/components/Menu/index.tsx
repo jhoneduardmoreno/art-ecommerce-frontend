2a37dae3767176ff4a77b517415c893061147e70
jhoneduardmoreno <jhoneduar1994@hotmail.com>
2024-04-13 12:25:39 -0500

    Add logo and shopping cart icon

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,19 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from '@mui/material';
+import styles from './Menu.module.scss';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export const Menu = () => {
   return (
     <div>
         <IconButton onClick={() => console.log('Opening menu')}>
             <MenuIcon sx={{fontSize: '3rem', color: '#000'}} />
+        </IconButton>
+        <div className={styles.logo}>
+            ISMARA DAYANA REINELL
+        </div>
+        <IconButton onClick={() => console.log('Opening cart')}>
+            <ShoppingCartIcon sx={{fontSize: '3rem', color: '#000'}} />
         </IconButton>
     </div>
   )
